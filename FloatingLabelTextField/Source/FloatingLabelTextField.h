4ab7fae99eb0f632ddb5ba99354b35da25a35f77
Eric Baker <eric.baker@etrade.com>
2017-11-08 12:47:07 -0600

    Create framework project

---
+++ FloatingLabelTextField/Source/FloatingLabelTextField.h
@@ -0,0 +1,19 @@
+//
+//  FloatingLabelTextField.h
+//  FloatingLabelTextField
+//
+//  Created by Eric Baker on 8/Nov/2017.
+//  Copyright © 2017 DuneParkSoftware, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FloatingLabelTextField.
+FOUNDATION_EXPORT double FloatingLabelTextFieldVersionNumber;
+
+//! Project version string for FloatingLabelTextField.
+FOUNDATION_EXPORT const unsigned char FloatingLabelTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FloatingLabelTextField/PublicHeader.h>
+
+
